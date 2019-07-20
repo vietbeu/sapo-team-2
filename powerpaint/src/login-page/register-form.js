e12da0407ea8dd8b56b74a4d47f9a727ec19cd37
vietbeu <vietbeu@users.noreply.github.com>
2019-07-20 14:05:57 +0700

    validate Phone

--- powerpaint/src/login-page/register-form.js
+++ powerpaint/src/login-page/register-form.js
@@ -2,10 +2,16 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 class RegisterForm extends Component {
-    state = {  }
+    state = { 
+        isPhoneValid: true,
+        errorPhoneMessage:'',
+     }
+
+
     clickForgetPass = () => {
         this.props.onClickForgetPass();
     }
+
     channgeName = (e) => {
         this.setState({name: e.target.value})
     }
@@ -21,6 +27,31 @@ class RegisterForm extends Component {
     changeConfirmPass = (e) => {
         this.setState({confirmPass: e.target.value})
     }
+
+
+    validatePhone = (checkingText) => {
+        const regexp = /^\d{10,11}$/;
+        const checkingResult = regexp.exec(checkingText);
+        if (checkingResult !== null) {
+            return { isPhoneValid: true,
+                     errorPhoneMessage: ''};
+        } else {
+            return { isPhoneValid: false,
+                     errorPhoneMessage: 'Số điện thoại phải có 10 - 11 chữ số.'};
+        }
+    }
+    validationPhone = (e) => {
+        const {isPhoneValid,errorPhoneMessage} = this.validatePhone(this.state.phone);
+        this.setState({
+            isPhoneValid: isPhoneValid,
+            errorPhoneMessage: errorPhoneMessage
+        })
+    }
+
+    validateEmail = (emailText) => {
+        
+    }
+    
     handleSubmit = (e) => {
         e.preventDefault();
         axios.post('http://192.168.9.253:8181/api/v1/registration', {
@@ -48,10 +79,15 @@ class RegisterForm extends Component {
                     <form onSubmit={this.handleSubmit}>
                         <div>
                             <input type='text' placeholder='Họ và tên' required onChange={this.channgeName} autoComplete='off'/>
-                            <input type='number' placeholder='Số điện thoại' autoComplete='off' onChange={this.changePhone}/>
-                            <input type='email' placeholder='Email' required autoComplete='off' onChange={this.changeEmail}/>
-                            <input type='password' placeholder='Mật khẩu' required autoComplete='off' onChange={this.changePass}/>
-                            <input type='password' placeholder='Nhập lại mật khẩu' required autoComplete='off' onChange={this.changeConfirmPass}/>
+                            <input type='number' placeholder='Số điện thoại' autoComplete='off' 
+                            onChange={this.changePhone} onBlur={this.validationPhone}/>
+                            <FormError isHidden={this.state.isPhoneValid} errorMessage={this.state.errorPhoneMessage} />
+                            <input type='email' placeholder='Email' required autoComplete='off' 
+                            onChange={this.changeEmail} onBlur={this.validationEmail}/>
+                            <input type='password' placeholder='Mật khẩu' required autoComplete='off' 
+                            onChange={this.changePass} onBlur={this.validationPass}/>
+                            <input type='password' placeholder='Nhập lại mật khẩu' required autoComplete='off' 
+                            onChange={this.changeConfirmPass} onBlur={this.validationConfirmPass}/>
                         </div>
                         <button className='submit' type='submit' >Đăng ký</button>
                         <button onClick={this.clickForgetPass} className='forget-pass'>Quên mật khẩu?</button>
@@ -64,3 +100,9 @@ class RegisterForm extends Component {
 }
  
 export default RegisterForm;
+
+
+const FormError=(props) =>{
+    if (props.isHidden) { return null;}
+    return ( <div>{props.errorMessage}</div>)
+}
