88b6f904ea33bf61d7068436c8fedafc6303b19b
vietbeu <vietbeu@users.noreply.github.com>
2019-07-23 11:22:23 +0700

    lam lai header

--- powerpaint/src/component/header.js
+++ powerpaint/src/component/header.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
 import '../css/header.css'
 import Popup from 'reactjs-popup'
+import Axios from 'axios';
 class Header extends Component {
     state = {  }
+    componentDidMount(){
+        document.getElementById('avatar').style.backgroundImage =
+        'url(https://ui-avatars.com/api/?rounded=true&size=50&background=27AE60&color=FFFFFF&name=Le+Viet)';
+
+    }
     render() { 
         return ( 
             <div className = 'header-content'>
@@ -14,17 +20,22 @@ class Header extends Component {
                     </span>
                 </div>
                
-                <div className='info'>
-                    <span></span>
+                <div className='info' id='avatar'>
                 </div>
-                <div className='help'>
-                    <div>
-                    <i className="fa fa-question-circle fa-2x" aria-hidden="true"></i>
+                <button className='header-button' onClick={() =>
+                    alert("Hãy liên hệ với chúng tôi qua  số điện thoại 0975867756 hoặc email sapoteam2@gmail.com")}>
+                    <div className='help'>
+                        <div>
+                            <span className="fa-stack fa-lg">
+                                <i className="fa fa-circle-o fa-stack-2x"></i>
+                                <i className="fa fa-question fa-stack-1x fa-inv "></i>
+                            </span>
+                        </div>
+                        <div id='help-label'>
+                        Trợ giúp
+                        </div>
                     </div>
-                    <div id='help-label'>
-                    Trợ giúp
-                    </div>
-                </div>
+                </button>
             </div>
          );
     }
